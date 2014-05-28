20d8cdbbff404d10aff69969cdacdd26390003f1
BennyZ <BennyZ@172.20.10.4>
2014-05-28 15:49:56 +0200

    Premier commit, Création base GitHub

---
+++ Jgarage/src/jgarage/Jgarage.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jgarage;
+
+/**
+ *
+ * @author BennyZ
+ */
+public class Jgarage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
